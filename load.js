3596fcdacacc0accbfc79a9988e08f81852af251
Saransak Lueangoon <145457395+SaransakL@users.noreply.github.com>
2026-05-03 01:30:53 +0700

    Add files via upload

--- load.js
+++ load.js
@@ -1,3 +1,15 @@
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => observer.observe(el));
+
 function toggleMenu() {
     document.getElementById("offcanvas").classList.toggle("active");
 }
